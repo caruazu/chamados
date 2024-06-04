32907ceb39a1c88a3f6f6ce5e6f7251a37dbec21
caruazu <gustavocaruazu@gmail.com>
2024-06-04 15:16:27 -0300

    navbar implementada

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,9 +1,23 @@
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
 const Nav = () => {
   return (
-    <div>
-      NAV
-    </div>
-  )
-}
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/ChamadoPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className=" text-default-text">gustavo@caruazu.com</p>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav
